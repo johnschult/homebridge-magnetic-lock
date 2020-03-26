174fdda2b0514f1c194426e73aedb08493eb0b5d
Schult, John (js798p) <js798p@att.com>
2020-03-26 10:36:07 -0400

    More refactoring

--- index.js
+++ index.js
@@ -3,83 +3,105 @@ let Service, Characteristic
 
 class MagneticLockAccessory {
   constructor (log, config) {
-    const { name, lockSwitchPin, unlockDurationInSecs, lockPollInSecs } = config
     this.log = log
-    this.name = name
-    this.lockSwitchPin = lockSwitchPin
-    this.unlockDurationInSecs = parseInt(unlockDurationInSecs)
-    this.lockPollInMs = parseInt(lockPollInSecs) * 1000
+    this.config = config
+    this.config.unlockDurationInSecs = parseInt(config.unlockDurationInSecs)
+    this.config.lockPollInMs = parseInt(config.lockPollInSecs) * 1000
+    const {
+      lockSwitchPin,
+      unlockDurationInSecs,
+      lockPollInMs,
+      lockPollInSecs
+    } = this.config
+
     log(`Lock Switch Pin: ${lockSwitchPin}`)
     log(`Unlock Duration in seconds: ${unlockDurationInSecs}`)
     log(`Lock poll in seconds: ${lockPollInSecs}`)
     this.initService()
-    setTimeout(this.monitorLockState.bind(this), this.lockPollInMs)
-  }
-
-  monitorLockState () {
-    const state = this.gpioRead()
-    this.currentLockState.setValue(state)
-    setTimeout(this.monitorLockState.bind(this), this.lockPollInMs)
+    setTimeout(this.monitorLockState.bind(this), lockPollInMs)
   }
 
   initService () {
-    this.magneticLock = new Service.LockMechanism(this.name)
+    const {
+      config: { name }
+    } = this
+    const { LockCurrentState, LockTargetState } = Characteristic
+
+    this.magneticLock = new Service.LockMechanism(name)
     this.currentLockState = this.magneticLock.getCharacteristic(
-      Characteristic.LockCurrentState
+      LockCurrentState
     )
-    this.currentLockState.on('get', this.getState.bind(this))
-    this.targetLockState = this.magneticLock.getCharacteristic(
-      Characteristic.LockTargetState
-    )
-    this.targetLockState
-      .on('get', this.getState.bind(this))
-      .on('set', this.setState.bind(this))
-    this.currentLockState.setValue(Characteristic.LockCurrentState.SECURED)
-    this.targetLockState.setValue(Characteristic.LockCurrentState.SECURED)
+    this.targetLockState = this.magneticLock.getCharacteristic(LockTargetState)
+
+    const { currentLockState, targetLockState } = this
+    const initialLockState = LockCurrentState.SECURED
+    const getState = this.getState.bind(this)
+    const setState = this.setState.bind(this)
+
+    currentLockState.on('get', getState)
+    targetLockState.on('get', getState).on('set', setState)
+
+    currentLockState.setValue(initialLockState)
+    targetLockState.setValue(initialLockState)
+  }
+
+  monitorLockState () {
+    const {
+      currentLockState,
+      monitorLockState,
+      config: { lockPollInMs }
+    } = this
+
+    currentLockState.setValue(this.gpioRead())
+    setTimeout(monitorLockState.bind(this), lockPollInMs)
   }
 
   gpioWrite (value) {
-    fs.writeFileSync(
-      '/sys/class/gpio/gpio' + this.lockSwitchPin + '/value',
-      value
-    )
+    const { lockSwitchPin } = this.config
+    fs.writeFileSync(`/sys/class/gpio/gpio${lockSwitchPin}/value`, value)
   }
 
   gpioRead () {
+    const { lockSwitchPin } = this.config
     return parseInt(
       fs
-        .readFileSync(
-          '/sys/class/gpio/gpio' + this.lockSwitchPin + '/value',
-          'utf8'
-        )
+        .readFileSync(`/sys/class/gpio/gpio${lockSwitchPin}/value`, 'utf8')
         .trim()
     )
   }
 
-  getState (callback) {
-    callback(null, this.gpioRead())
+  getState (cb) {
+    cb(null, this.gpioRead())
   }
 
-  setState (state, callback) {
-    this.log(`Setting state to ${state}`)
+  setState (state, cb) {
+    const {
+      currentLockState,
+      log,
+      targetLockState,
+      unlockDurationInSecs
+    } = this
+
+    log(state ? 'Locking' : 'Unlocking')
+
     switch (state) {
       case Characteristic.LockCurrentState.UNSECURED:
         this.gpioWrite(state)
         setTimeout(
           function () {
             const lockedState = Characteristic.LockCurrentState.SECURED
-            this.log(`Automatically setting state to ${lockedState}`)
-            this.targetLockState.setValue(lockedState)
-            this.currentLockState.setValue(lockedState)
+            log('Auto-locking')
+            targetLockState.setValue(lockedState)
+            currentLockState.setValue(lockedState)
           }.bind(this),
-          this.unlockDurationInSecs * 1000
+          unlockDurationInSecs * 1000
         )
         break
       default:
         this.gpioWrite(state)
-        this.currentLockState.setValue(state)
+        currentLockState.setValue(state)
     }
-    callback()
+    cb()
     return true
   }
 
